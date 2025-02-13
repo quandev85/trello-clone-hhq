add1c4ca6915de4f2f79452688e3e5ad79b39168
quandev85 <hhq851999@gmail.com>
2025-02-13 17:45:26 +0700

    Setup project

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { CssBaseline } from '@mui/material';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <CssBaseline />
+    <App />
+  </StrictMode>
+);
